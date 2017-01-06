599d6c94d72979b74fd988901fcbab89c3a2945d
Xavier Cambar <xcambar@gmail.com>
2017-01-06 15:35:33 +0100

    Downloads Crowdin translations with production builds

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 module.exports = {
   name: 'ember-cli-crowdin',
+  preBuild: function() {
+    if (!this.app.isProduction) {
+      return;
+    }
+    return require('./lib/commands/download').run.call(this);
+  },
   includedCommands: function() {
     return {
       'crowdin:check': require('./lib/commands/check'),
